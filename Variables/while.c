f2691d28574515594f24af1a39006b2fdb95f254
Shakil00111 <Shakil115204@gmail.com>
2025-01-31 12:13:21 +0600

    While loop structure

---
+++ Variables/while.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+    int i = 0;
+
+    while(i < 10) {
+        if(i == 9) {
+            printf("LORD");
+        } else {
+            printf("LORD, ");
+        }
+
+        i++;
+    }
+
+    return 0;
+}
